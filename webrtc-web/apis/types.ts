9ebcc76aeaf10c22ad31a01c9df421715dc6a735
duststars <i@exploro.one>
2026-02-20 14:14:07 +0800

    remove comments

--- webrtc-web/apis/types.ts
+++ webrtc-web/apis/types.ts
@@ -10,32 +10,10 @@ export type Preference = {
   indexOfPreferColor: number;
 };
 
-// type RenamePayload struct {
-// 	NewNodeName    string `json:"new_node_name"`
-// 	OriginNodeName string `json:"origin_node_name,omitempty"`
-// }
 export interface RenamePayload {
   new_preference: Preference;
   origin_node_name?: string;
 }
-
-// type MessagePayload struct {
-// 	NodeId   string                      `json:"node_id,omitempty"`
-// 	Register *pkgconnreg.RegisterPayload `json:"register,omitempty"`
-// 	Echo     *pkgconnreg.EchoPayload     `json:"echo,omitempty"`
-
-// 	// emit when some node changes its name
-// 	Rename *pkgconnreg.RenamePayload `json:"rename,omitempty"`
-
-// 	// emit when node goes online
-// 	Online *pkgconnreg.NodeGoesOnline `json:"online,omitempty"`
-
-// 	// SDP and ICE offers are concepts from WebRTC
-// 	SDPOffer *pkgconnreg.SDPOfferPayload `json:"sdp_offer,omitempty"`
-// 	ICEOffer *pkgconnreg.ICEOfferPayload `json:"ice_offer,omitempty"`
-
-// 	AttributesAnnouncement *pkgconnreg.AttributesAnnouncementPayload `json:"attributes_announcement,omitempty"`
-// }
 
 export interface MessagePayload {
   node_id?: string;
@@ -52,32 +30,16 @@ export interface MessagePayload {
   attributes_announcement?: AttributesAnnouncementPayload;
 }
 
-// type RegisterPayload struct {
-//  	NodeName string  `json:"node_name"`
-//  	Token    *string `json:"token,omitempty"`
-// }
 export interface RegisterPayload {
   node_name: string;
   token?: string;
 }
 
-// type EchoDirection string
-// const (
-//  	EchoDirectionC2S EchoDirection = "ping"
-//  	EchoDirectionS2C EchoDirection = "pong"
-// )
 export type EchoDirection = "ping" | "pong";
 
 export const EchoDirectionC2S = "ping" as const;
 export const EchoDirectionS2C = "pong" as const;
 
-// type EchoPayload struct {
-//  	Direction       EchoDirection `json:"direction"`
-//  	CorrelationID   string        `json:"correlation_id"`
-//  	ServerTimestamp uint64        `json:"server_timestamp"`
-//  	Timestamp       uint64        `json:"timestamp"`
-//  	SeqID           uint64        `json:"seq_id"`
-// }
 export interface EchoPayload {
   direction: EchoDirection;
   correlation_id: string;
@@ -86,35 +48,16 @@ export interface EchoPayload {
   seq_id: number;
 }
 
-// type AttributesAnnouncementPayload struct {
-//  	Attributes  ConnectionAttributes `json:"attributes,omitempty"`
-//  	Withdrawals []string             `json:"withdrawals,omitempty"`
-// }
 export interface AttributesAnnouncementPayload {
   attributes?: ConnectionAttributes;
   withdrawals?: string[];
 }
 
-// type AuthenticationType string
-// const (
-// 	AuthenticationTypeJWT  AuthenticationType = "jwt"
-// 	AuthenticationTypeMTLS AuthenticationType = "mtls"
-// )
 export type AuthenticationType = "jwt" | "mtls";
 
 export const AuthenticationTypeJWT = "jwt" as const;
 export const AuthenticationTypeMTLS = "mtls" as const;
 
-// type ConnRegistryData struct {
-// 	NodeName       *string              `json:"node_name,omitempty"`
-// 	ConnectedAt    uint64               `json:"connected_at"`
-// 	RegisteredAt   *uint64              `json:"registered_at,omitempty"`
-// 	LastHeartbeat  *uint64              `json:"last_heartbeat,omitempty"`
-// 	Attributes     ConnectionAttributes `json:"attributes,omitempty"`
-// 	QUICConn       *quicGo.Conn         `json:"-"`
-// 	Claims         jwt.MapClaims        `json:"-"`
-// 	Authentication AuthenticationType   `json:"authentication"`
-// }
 export interface ConnRegistryData {
   node_name?: string;
   connected_at: number;
